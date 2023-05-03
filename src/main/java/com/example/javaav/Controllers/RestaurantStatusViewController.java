bb8765ce5fcfce9fa4b368c9d977de02855af68b
N0ku <lucielecouturier2@gmail.com>
2023-05-03 14:51:25 +0200

    🔨 - Init data restaurant, start a template for the RestaurantStatusView.fxml and display data of the tables and the customers

--- src/main/java/com/example/javaav/Controllers/RestaurantStatusViewController.java
+++ src/main/java/com/example/javaav/Controllers/RestaurantStatusViewController.java
@@ -1,4 +1,63 @@
 package com.example.javaav.Controllers;
 
-public class RestaurantStatusViewController {
+import com.example.javaav.HelloApplication;
+import com.example.javaav.Model.Customers;
+import com.example.javaav.Model.Restaurant;
+import com.example.javaav.Model.Tables;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class RestaurantStatusViewController implements Initializable {
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private ListView<String> customersList;
+
+    @FXML
+    private ListView<String> tablesList;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        backButton.setOnMouseClicked(event ->{
+            try {
+                Parent root = FXMLLoader.load((Objects.requireNonNull(getClass().getResource("/com/example/javaav/DashboardView.fxml"))));
+                Scene currentScene = backButton.getScene();
+                currentScene.setRoot(root);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Restaurant restaurant = HelloApplication.restaurant;
+
+        ArrayList<Customers> customers = restaurant.getCustomersList();
+        ArrayList<Tables> tables = restaurant.getTablesList();
+
+        customers.forEach(customer -> {
+            customersList.getItems().add(customer.getName());
+        });
+
+        tables.forEach(
+                table ->{
+                    tablesList.getItems().add("Table: " +  table.getTableNumber());
+                }
+        );
+
+    }
 }
