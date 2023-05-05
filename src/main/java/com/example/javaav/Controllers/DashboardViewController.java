93d4962ecf1c3bcf20a2b8db86641c1fd8b327d7
N0ku <lucielecouturier2@gmail.com>
2023-05-05 03:10:58 +0200

    💎- Update dashboard

--- src/main/java/com/example/javaav/Controllers/DashboardViewController.java
+++ src/main/java/com/example/javaav/Controllers/DashboardViewController.java
@@ -19,8 +19,18 @@ import java.util.stream.Collectors;
 
 public class DashboardViewController implements Initializable {
 
+
     @FXML
-    private ListView<Employees> EmployeesList;
+    private ListView<Employees> EmployeesListl30;
+
+    @FXML
+    private ListView<Employees> EmployeesListl45;
+
+    @FXML
+    private ListView<Employees> EmployeesListm30;
+
+    @FXML
+    private ListView<Employees> EmployeesListm45;
 
     @FXML
     private ListView<Meals> MealList;
@@ -130,6 +140,21 @@ public class DashboardViewController implements Initializable {
                 .sorted(Comparator.comparing(Orders::getHour).reversed())
                 .collect(Collectors.toCollection(ArrayList::new));
 
+        ArrayList<Employees> more30 = restaurant.getEmployeesList().stream().filter(e -> e.getAge() >= 30).collect(Collectors.toCollection(ArrayList::new));
+        ArrayList<Employees> less30 = restaurant.getEmployeesList().stream().filter(e -> e.getAge() < 30).collect(Collectors.toCollection(ArrayList::new));
+        ArrayList<Employees> more45 = restaurant.getEmployeesList().stream().filter(e -> e.getAge() >= 45).collect(Collectors.toCollection(ArrayList::new));
+        ArrayList<Employees> less45 = restaurant.getEmployeesList().stream().filter(e -> e.getAge() < 45).collect(Collectors.toCollection(ArrayList::new));
+
+        EmployeesListl30.getItems().addAll(less30);
+        EmployeesListl45.getItems().addAll(less45);
+        EmployeesListm30.getItems().addAll(more30);
+        EmployeesListm45.getItems().addAll(more45);
+
+        EmployeesListl30.setCellFactory(e -> new CellEmployees());
+        EmployeesListm30.setCellFactory(e -> new CellEmployees());
+        EmployeesListl45.setCellFactory(e -> new CellEmployees());
+        EmployeesListm45.setCellFactory(e -> new CellEmployees());
+
 
         ordersWaitingList.getItems().addAll(ordersWaiting);
         ordersWaitingList.setCellFactory(o -> new CellOrders());
@@ -148,6 +173,23 @@ public class DashboardViewController implements Initializable {
                 .flatMap(customer -> customer.getOrders().stream())
                 .reduce(0, (result, order) -> (int) (result + order.getTotalPrice()), Integer::sum);
         factl.setText(totalMoneyCL + " €");
+
+        int totalMealsPrice = restaurant.getMealsList().stream().reduce(0,(result,meal) -> (int) (result + meal.getPrice()),Integer::sum);
+        totalPrice.setText(totalMealsPrice + " €");
+
+        Meals mealWithHighestPrice = restaurant.getMealsList().stream()
+                .max(Comparator.comparing(Meals::getPrice))
+                .orElse(null);
+
+        assert mealWithHighestPrice != null;
+        topPrice.setText("Name: " + mealWithHighestPrice.getName() + " Price: " +  mealWithHighestPrice.getPrice() +  " €");
+
+        Meals mealWithLowestPrice = restaurant.getMealsList().stream()
+                .min(Comparator.comparing(Meals::getPrice))
+                .orElse(null);
+
+        assert mealWithLowestPrice != null;
+        lowPrice.setText("Name: " + mealWithLowestPrice.getName() + " Price: " + mealWithLowestPrice.getPrice() + " €");
 
     }
 }
